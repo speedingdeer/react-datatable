6d9a0eb04fc97259bc5b3c6902aa54f97452b945
speedingdeer <ffwisniewski@gmail.com>
2018-01-02 17:29:06 +0100

    Abstracted fake data creation to util. Used fake data for test scaffold.

--- src/components/DataTable/DataTable.test.js
+++ src/components/DataTable/DataTable.test.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import DataTable from './index';
 
+import {fakeData} from '../../util';
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<DataTable columns={[]} data={[]}/>, div);
+  ReactDOM.render(<DataTable {...fakeData()}/>, div);
 });
--- src/containers/Demo/index.js
+++ src/containers/Demo/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Container } from 'semantic-ui-react';
-import faker from 'faker';
 import DataTable from '../../components/DataTable';
+import {fakeData} from '../../util';
 
 
 class Demo extends Component {
@@ -34,39 +34,7 @@ class Demo extends Component {
 
   componentDidMount() {
 
-    this.setState({
-        columns: [{
-          attribute: 'field_name',
-          label: 'Name'
-        }, {
-          attribute: 'cultivation_id',
-          label: 'Nummer'
-        }, {
-          attribute: 'company_name',
-          label: 'Betrieb'
-        }, {
-          attribute: 'area_in_hectares',
-          label: 'Fläche'
-        }, {
-          attribute: 'active',
-          label: 'Aktiv'
-        }, {
-          attribute: 'crop_name',
-          label: 'Kultur'
-        }, {
-          attribute: 'harvest_year',
-          label: 'Ernte'
-        }],
-        data: Array(50 + faker.random.number(50)).fill().map((_, i) => { return {
-          field_name: faker.name.findName(),
-          area_in_hectares: parseFloat(( 1 + faker.random.number(20) ) / ( 1 + faker.random.number(20) )).toFixed(2),
-          active: faker.random.boolean(),
-          company_name: faker.company.companyName(),
-          cultivation_id: i,
-          crop_name: faker.name.findName(),
-          harvest_year: 2000 + faker.random.number(18)
-        }})
-      });
+    this.setState({...fakeData()})
     }
 }
 
