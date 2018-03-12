07ab9db9c6270b130fdb41131f6ab01e45e898f5
speedingdeer <ffwisniewski@gmail.com>
2018-03-12 19:18:12 +0100

    Made router github readable.

--- src/routes.js
+++ src/routes.js
@@ -7,9 +7,9 @@ import DemoFullScreen from './containers/DemoFullScreen';
 const Routes = () => (
   <BrowserRouter>
     <Switch>
-      <Route exact path='/' component={Home}/>
-      <Route exact path='/demo' component={Demo}/>
-      <Route exact path='/demo-full-screen' component={DemoFullScreen}/>
+      <Route exact path={`${process.env.PUBLIC_URL}/`} component={Home}/>
+      <Route exact path={`${process.env.PUBLIC_URL}/demo`} component={Demo}/>
+      <Route exact path={`${process.env.PUBLIC_URL}/demo-full-screen`} component={DemoFullScreen}/>
     </Switch>
   </BrowserRouter>
 )
